c0f45ed5733f09af58b69b8cc6a44f7bfa81cc12
loibq1993 <buiquangloi1993@gmail.com>
2019-09-17 09:51:40 +0700

    finish Async writes in redux

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -9,9 +9,31 @@ class ManageCoursePage extends React.Component {
         super(props, context);
 
         this.state = {
-            course: Object.assign({}, this.props.course),
+            course: Object.assign({}, props.course),
             errors: {}
         }
+
+        this.updateCourseState = this.updateCourseState.bind(this);
+        this.saveCourse = this.saveCourse.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.course.id != nextProps.course.id) {
+            this.setState({course: Object.assign({}, nextProps.course)})
+        }
+    }
+
+    updateCourseState(event) {
+        const field = event.target.name;
+        let course = this.state.course;
+        course[field] = event.target.value;
+        return this.setState({course: course});
+    }
+
+    saveCourse(event) {
+        event.preventDefault();
+        this.props.actions.saveCourse(this.state.course);
+        this.context.router.push('/courses')
     }
 
     render() {
@@ -19,7 +41,9 @@ class ManageCoursePage extends React.Component {
             <div>
                 <h1>Manage Course</h1>
                 <CourseForm 
-                allAuthors={[]}
+                allAuthors={this.props.authors}
+                onChange={this.updateCourseState}
+                onSave={this.saveCourse}
                 course={this.state.course}
                 errors={this.state.errors}
                 />
@@ -28,13 +52,31 @@ class ManageCoursePage extends React.Component {
     }
 }
 
-ManageCoursePage.PropTypes = {
+ManageCoursePage.propTypes = {
     course: PropTypes.object.isRequired,
-    authors: PropTypes.array.isReqruied
+    authors: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
+ManageCoursePage.contextTypes = {
+    router: PropTypes.object.isRequired
+}
+
+function getCourseById(courses, id) {
+    const course = courses.filter(course => course.id == id);
+    if (course) return course[0];
+    return null;
+}
+
 function mapStateToProps(state, ownProps) {
-    let course = {id: '', watchHref: '', title: '', author: '', length:'', category:''}
+    const courseId = ownProps.params.id;
+
+    let course = {id: '', watchHref: '', title: '', author: '', length:'', category:''};
+
+    if (courseId && state.courses.length > 0) {
+        course = getCourseById(state.courses, courseId);
+    }
+
     const authorsFormatterdForDropdown = state.authors.map(author => {
         return {
             value: author.id,
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,11 +2,21 @@ import * as types from '../actions/actionType';
 import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) { //export default can import any. if not default so have to use {importState}
-    switch (action.type) {
+switch (action.type) {
         case types.LOAD_COURSES_SUCCESS:
             // state.push(action.course);
             return action.courses;
-        
+        case types.CREATE_AUTHORS_SUCCESS:
+            return [
+                ...state,
+                Object.assign({}. action.courses)
+            ]
+        case types.UPDATE_COURSES_SUCCESS:
+            return [
+                ...state.filter(course => course.id !== action.courses.id),
+                Object.assign({}, action.courses)
+            ];
+
         default:
             return state;
     }
